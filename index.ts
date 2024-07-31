18e9337a08b504c06cd84956bdb338b86b487c2a
Aashish Chapain <ashishchapain86@gmail.com>
2024-07-31 07:53:58 +0545

    added cors origin

--- index.ts
+++ index.ts
@@ -7,7 +7,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000; 
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
 app.use("/", contactsRouter);
 
 app.listen(PORT, () => {
